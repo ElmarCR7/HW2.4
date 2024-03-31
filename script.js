d05feb530adc1033f0afd9dbd2b1da96318f37b2
ElmarCR7 <johncino082@gmail.com>
2024-03-31 19:21:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function isEven(number) {
+    return number % 2 === 0
+    
+}
+console.log(isEven(8));
+console.log(isEven(9));
+
+
+
+
+
+
+
+// {Dop HW}
+
+
+
+// {1}
+const numbers = [1, 2, 3, 4,5]
+let sum = 0
+for (let i = 0;  i < numbers.length; i++) {
+    sum += numbers[i]
+}
+
+console.log(sum);
+// {2}
+
+
+const number = [10, 20, 30]
+let summ = 0
+for (let i = 0;  i < number.length; i++) {
+    summ += number[i]
+}
+
+console.log(summ);
+
+    
